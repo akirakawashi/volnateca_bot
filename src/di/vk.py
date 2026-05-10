d88ba2b6789c2a2fc302b13ea762831a12f4cc47
akirakawashi <mr.oldman.23@gmail.com>
2026-05-10 08:56:45 +0300

    refactor: Update VKProvider to include IVKMessageClient and adjust client retrieval methods

--- src/di/vk.py
+++ src/di/vk.py
@@ -2,16 +2,24 @@ from collections.abc import AsyncIterable
 
 from dishka import Provider, Scope, provide
 
-from application.interface.clients import IVKUserClient
+from application.interface.clients import IVKMessageClient, IVKUserClient
 from infrastructure.vk import VKAPIClient
 from settings.vk import VKSettings
 
 
 class VKProvider(Provider):
-    @provide(scope=Scope.APP, provides=IVKUserClient)
-    async def get_vk_user_client(
+    @provide(scope=Scope.APP)
+    async def get_vk_api_client(
         self,
         settings: VKSettings,
     ) -> AsyncIterable[VKAPIClient]:
         async with VKAPIClient(settings=settings) as client:
             yield client
+
+    @provide(scope=Scope.APP, provides=IVKUserClient)
+    def get_vk_user_client(self, client: VKAPIClient) -> IVKUserClient:
+        return client
+
+    @provide(scope=Scope.APP, provides=IVKMessageClient)
+    def get_vk_message_client(self, client: VKAPIClient) -> IVKMessageClient:
+        return client
