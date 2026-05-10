27cfd7869969faf0eddd788ed127121ebdafe24c
akirakawashi <mr.oldman.23@gmail.com>
2026-05-10 08:56:31 +0300

    refactor: Include IVKMessageClient in __all__ for improved client accessibility

--- src/application/interface/clients/__init__.py
+++ src/application/interface/clients/__init__.py
@@ -1,3 +1,6 @@
-from application.interface.clients.vk import IVKUserClient
+from application.interface.clients.vk import IVKMessageClient, IVKUserClient
 
-__all__ = ["IVKUserClient"]
+__all__ = [
+    "IVKMessageClient",
+    "IVKUserClient",
+]
